c37fbd4f764f84e46b0fe372fbae7c96fa7da12e
studying-asyouwere <studyingasyouwere@gmail.com>
2022-12-03 19:45:03 +1300

    day 3

--- all-solutions.py
+++ all-solutions.py
@@ -94,7 +94,40 @@ for game in games:
 print('The total score of the game using the first strategy is: ' + str(total_score_first))
 print('The total score of the game using the second strategy is: ' + str(total_score_second))
 
+# Day 3
 
+with open('input.txt', 'r') as file:
+    data = file.read().strip()
+
+rucksacks = data.split('\n')
+
+priority_q1 = 0
+for rs in rucksacks:
+    L = len(rs)
+    comp1 = rs[:L // 2]
+    comp2 = rs[L // 2:]
+    for item in comp1:
+        if item in comp2:
+            value = ord(item) - 96 if item.islower() else ord(item) - 64 + 26
+            priority_q1 += value
+            break
+
+print('The priority for the first task is: ' + str(priority_q1))
+
+ii = 0
+priority_q2 = 0
+while ii < len(rucksacks):
+    bag1 = rucksacks[ii]
+    bag2 = rucksacks[ii + 1]
+    bag3 = rucksacks[ii + 2]
+    for item in bag1:
+        if item in bag2 and item in bag3:
+            value = ord(item) - 96 if item.islower() else ord(item) - 64 + 26
+            priority_q2 += value
+            ii += 3
+            break
+
+print('The priority for the second task is: ' + str(priority_q2))
 
 
 
--- main.py
+++ main.py
@@ -1,79 +1,32 @@
-with open('input.txt') as file:
-    file.seek(0)
-    games = file.read()
+with open('input.txt', 'r') as file:
+    data = file.read().strip()
 
-games = games.split("\n")
-games = games[:-1]
+rucksacks = data.split('\n')
 
-# First strategy
-total_score_first = 0
-for game in games:
-    game_score = 0
-    opponent_hand = game[0]
-    my_hand = game[2]
-    if my_hand == 'X':
-        game_score += 1
-        if opponent_hand == 'A':
-            game_score += 3
-        elif opponent_hand == 'B':
-            game_score += 0
-        elif opponent_hand == 'C':
-            game_score += 6
-    elif my_hand == 'Y':
-        game_score += 2
-        if opponent_hand == 'A':
-            game_score += 6
-        elif opponent_hand == 'B':
-            game_score += 3
-        elif opponent_hand == 'C':
-            game_score += 0
-    elif my_hand == 'Z':
-        game_score += 3
-        if opponent_hand == 'A':
-            game_score += 0
-        elif opponent_hand == 'B':
-            game_score += 6
-        elif opponent_hand == 'C':
-            game_score += 3
-    total_score_first += game_score
-    
-# Second strategy
-total_score_second = 0
-for game in games:
-    game_score = 0
-    opponent_hand = game[0]
-    my_strat = game[2]
-    if my_strat == 'X':
-        if opponent_hand == 'A':
-            game_score += 3
-        elif opponent_hand == 'B':
-            game_score += 1
-        elif opponent_hand == 'C':
-            game_score += 2
-    elif my_strat == 'Y':
-        game_score += 3
-        if opponent_hand == 'A':
-            game_score += 1
-        elif opponent_hand == 'B':
-            game_score += 2
-        elif opponent_hand == 'C':
-            game_score += 3
-    elif my_strat == 'Z':
-        game_score += 6
-        if opponent_hand == 'A':
-            game_score += 2
-        elif opponent_hand == 'B':
-            game_score += 3
-        elif opponent_hand == 'C':
-            game_score += 1
-    total_score_second += game_score
+priority_q1 = 0
+for rs in rucksacks:
+    L = len(rs)
+    comp1 = rs[:L // 2]
+    comp2 = rs[L // 2:]
+    for item in comp1:
+        if item in comp2:
+            value = ord(item) - 96 if item.islower() else ord(item) - 64 + 26
+            priority_q1 += value
+            break
 
-print('The total score of the game using the first strategy is: ' + str(total_score_first))
-print('The total score of the game using the second strategy is: ' + str(total_score_second))
+print('The priority for the first task is: ' + str(priority_q1))
 
+ii = 0
+priority_q2 = 0
+while ii < len(rucksacks):
+    bag1 = rucksacks[ii]
+    bag2 = rucksacks[ii + 1]
+    bag3 = rucksacks[ii + 2]
+    for item in bag1:
+        if item in bag2 and item in bag3:
+            value = ord(item) - 96 if item.islower() else ord(item) - 64 + 26
+            priority_q2 += value
+            ii += 3
+            break
 
-
-
-
-
-
+print('The priority for the second task is: ' + str(priority_q2))
