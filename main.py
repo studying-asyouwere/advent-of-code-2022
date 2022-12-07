5c64b463e0bc9a69b57d763cf62e65c92c0007eb
studying-asyouwere <studyingasyouwere@gmail.com>
2022-12-07 20:47:33 +1300

    day 7

--- main.py
+++ main.py
@@ -1,50 +1,60 @@
-import re, copy
+import os
 
 with open('input.txt', 'r') as file:
-    stack_txt, instruction_data = file.read().split('\n\n')
-    stack_txt = stack_txt.split('\n')
-    instruction_data = instruction_data.split('\n')
+    data = file.read().strip().split('\n')
 
-stack_last = stack_txt.pop()
+# initialise dicts
+subdirs = {} # store the subdirectories here
+direct_directory_size = {} # store the directory sizes here (excluding all the subdirectories)
 
-stack = {}
-loc = {}
-ordering = []
-for ii in range(len(stack_last)):
-    if stack_last[ii] != ' ':
-        stack[stack_last[ii]] = []
-        loc[stack_last[ii]] = ii
-        ordering.append(stack_last[ii])
+# extracting the folder structure
+for line in data:
+    if line[0] == '$':
+        ds, cmd, *ddir = line.split()
+        if cmd == 'cd':
+            path = ddir[0]
+            if path == '/': # for the top directory
+                curdir = path # this is the current directory
+            else:
+                curdir = os.path.normpath(os.path.join(curdir, path))
+            if curdir not in subdirs: # if we have not seen our current directory before
+                subdirs[curdir] = []
+                direct_directory_size[curdir] = 0
 
-for line in reversed(stack_txt):
-    for key in loc.keys():
-        if line[loc[key]] != ' ':
-            stack[key].append(line[loc[key]])
+    else: # if we get 'dir' or numbers instead of '$'
+        fsize, fname = line.split()
+        if fsize != 'dir': # if we get numbers
+            direct_directory_size[curdir] += int(fsize)
+        else: # if we get a new directory
+            subdirs[curdir].append(os.path.normpath(os.path.join(curdir, fname)))
 
-stack2 = copy.deepcopy(stack)
+# size calculation function
+def compute_dirsize(dirname: str):
+    dirsize = direct_directory_size[dirname] # initialise the size by the size of the directory excluding the subdirectories
+    for ii in subdirs[dirname]: # go through all the subdirectories
+        if ii in subdirs:
+            dirsize += compute_dirsize(ii) # looping the subdirectory size addition until we no longer find the subdirectories
+    return dirsize
 
-for line in instruction_data:
-    if 'move' in line:
-        inst_values = re.findall(r'(\d+)', line)
-        count = int(inst_values[0])
-        ff = inst_values[1]
-        tt = inst_values[2]
+# Part 1: Sum of all the directories with a total size of at most 100000.
+sol_pt1 = 0
+for ii in subdirs:
+    dirsize = compute_dirsize(ii)
+    if dirsize <= 100000:
+        sol_pt1 += dirsize
+print('The answer to the first questions is: ' + str(sol_pt1))
 
-        for ii in range(count):
-            pop_val = stack[ff].pop()
-            stack[tt].append(pop_val)
+# Part 2: The smallest possible file to delete to free up the required space.
+total_space = 70000000
+space_required = 30000000
+space_used = compute_dirsize('/')
 
-        stack2[tt] += stack2[ff][-count:]
-        stack2[ff] = stack2[ff][:-count]
-
-print('The answer for the first part is:')
-for ii in ordering:
-    print(stack[ii][-1], end = '')
-print()
-
-print('The answer for the second part is:')
-for ii in ordering:
-    print(stack2[ii][-1], end = '')
+delete_this_directory = total_space
+for ii in direct_directory_size:
+    dirsize = compute_dirsize(ii)
+    if dirsize >= space_required - (total_space - space_used) and dirsize <= delete_this_directory:
+        delete_this_directory = dirsize
+print('The answer to the first questions is: ' + str(delete_this_directory))
 
 
 
