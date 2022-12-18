060de2acfcec0adcac56d7e236971b18d6249781
studying-asyouwere <studyingasyouwere@gmail.com>
2022-12-18 18:54:28 +1300

    d17

--- all-solutions.py
+++ all-solutions.py
@@ -724,7 +724,264 @@ with open("input.txt", 'r') as file:
 # can you hear my computer? running like crazy right now....
 print('Part 2: the solution is: ' + str(mx * ans_x + ans_y))   
 
-        
+# Day 16
+
+import collections as c, itertools, re, functools
+
+r = r'Valve (\w+) .*=(\d*); .* valves? (.*)'
+
+V, F, D = set(), dict(), c.defaultdict(lambda: 1000)
+
+for v, f, us, in re.findall(r, open('input.txt').read()):
+    V.add(v) # store node here
+    if f != '0': F[v] = int(f) # stroe flow here
+    for u in us.split(', ') : D[u, v] = 1
+
+for k, i, j in itertools.product(V, V, V):
+    D[i, j] = min(D[i, j], D[i, k] + D[k, j])
+
+@functools.cache
+def search(t, u = 'AA', vs = frozenset(F), e = False):
+    tt = max([F[v] * (t - D[u, v] - 1) + search(t - D[u, v] - 1, v, vs - {v}, e)
+    for v in vs if D[u, v] < t] + [search(26, vs = vs) if e else 0])
+    return tt
+
+print('Part 1: the most pressure you can release is: ' + str(search(30)))
+print('Part 2: the most pressure you and the elephant can release is: ' + str(search(26, e = True)))
+
+# Day 17
+import collections
+
+def read_input(path: str = 'input.txt'):
+    with open(path) as file:
+        pattern = file.read().rstrip()
+    return pattern
+
+# pattern generator fun
+def pattern_generator(pattern: str):
+    idx = 0
+    n = len(pattern)
+    while True:
+        yield pattern[idx], idx
+        idx = (idx + 1) % n
+
+# Rock class
+class Rock:
+
+    def __init__(self, rock_type: str, x_offset: int, y_offset: int = 2):
+
+        # rock tye init ("-", "+", "L", "I", "o")
+        if rock_type == '-':
+            self.positions = [[x, y_offset] for x in range(x_offset, x_offset + 4)]
+        elif rock_type == '+':
+            self.positions = [[x_offset + 1, y_offset + 2], [x_offset, y_offset + 1], [x_offset + 1, y_offset + 1],
+            [x_offset + 2, y_offset + 1], [x_offset + 1, y_offset]]
+        elif rock_type == 'L':
+            self.positions = [[x_offset + 2, y_offset + 2], [x_offset + 2, y_offset + 1]] \
+                + [[x, y_offset] for x in range(x_offset, x_offset + 3)]
+        elif rock_type == 'I':
+            self.positions = [[x_offset, y] for y in range(y_offset + 3, y_offset - 1, -1)]
+        elif rock_type == 'o':
+            self.positions = [[x_offset, y_offset + 1], [x_offset + 1, y_offset + 1], [x_offset, y_offset],
+            [x_offset + 1, y_offset]]
+        else: 
+            raise NotImplementedError(f'{rock_type} is low key not recognisable.')
+
+        # rock type
+        self.type = rock_type
+
+    # fun to return the position in tuple
+    def get_positions(self) -> list[tuple[int]]:
+        return [tuple(position) for position in self.positions]
+
+    # fun to move the rock
+    def move(self, dx, dy):
+        for coords in self.positions: 
+            coords[0] += dx
+            coords[1] += dy
+
+# Cave class
+class Cave:
+    
+    def __init__(self, wind_pattern: str, rock_pattern: str = '-+LIo', width = 7):
+        self.width = width
+        self.occupied = set()
+        self.highetst = -1
+        self.falling_rock = None
+        self.wind_generator = pattern_generator(wind_pattern)
+        self.rock_type_generator = pattern_generator(rock_pattern)
+        self.rock_counter = 0
+        self.state_cache = collections.defaultdict(list) # store the state here for cycle detection
+
+    # spawn rocks down the cave
+    def _spawn_rock(self):
+        self.falling_rock = Rock(next(self.rock_type_generator)[0], 2, self.highetst + 4)
+
+    # place rocks
+    def _place_rock(self, rock: Rock):
+        # rock position
+        positions = rock.get_positions()
+
+        # latest cave height
+        self.highetst = max(self.highetst, *[position[1] for position in positions])
+
+        # latest occupied rocks
+        self.occupied.update(positions)
+
+        # rock counter up 
+        self.rock_counter += 1
+
+        # no more falling rock
+        self.falling_rock = None
+
+    # has the rock colllided in the cave
+    def _check_rock_collision(self, rock: Rock):
+        return any(self._check_collision(*position) for position in rock.get_positions())
+
+    def _check_collision(self, x: int, y:int):
+        return x < 0 or x > self.width - 1 or y < 0 or (x, y) in self.occupied
+
+    # check if a row is completely blocked in the tetris style
+    def highest_row_blocked(self):
+        return all((x, self.highest) in self.occupied for x in range(0, self.width))
+
+    # fun to detect cycles
+    def detect_cycles(self) -> tuple[tuple[int, int], int, int]:
+        cycles = []
+
+        for value in self.state_cache.values():
+            if len(value) > 1:
+                cycles = value
+                break
+
+        if not cycles:
+            return (-1, -1), -1, -1
+
+        return cycles[0], cycles[1][0] - cycles[0][0], cycles[1][1] - cycles[0][1]
+
+    # get wind direction from strings
+    @staticmethod
+    def _get_direction_from_string(direction: str, inverse: bool = False):
+
+        # translate the direction if inverse
+        if inverse: 
+            if direction == '>':
+                direction = '<'
+            elif direction == '<':
+                direction = '>'
+            elif direction == 'v':
+                direction = '^'
+            elif direction == '^':
+                direction = 'v'
+            else:
+                raise NotImplementedError
+
+        # go through the cases
+        if direction == '>':
+            dx = 1
+            dy = 0
+        elif direction == '<':
+            dx = -1
+            dy = 0
+        elif direction == 'v':
+            dx = 0
+            dy = -1
+        elif direction == '^':
+            dx = 0
+            dy = 1
+        else: 
+            raise NotImplementedError
+
+        return dx, dy
+
+    def _get_surface_profile(self):
+        # the first element blocking each column
+        profile = []
+        for x in range(0, self.width):
+            y = self.highetst
+            while not self._check_collision(x, y):
+                y -= 1
+            profile.append(self.highetst - y)
+        return tuple(profile)
+
+    # implement the steps
+    def step(self):
+        # get the current wind
+        wind, wind_idx = next(self.wind_generator)
+
+        # init placed
+        placed = False
+
+        # do we have a falling rock or should one start fallling
+        if not self.falling_rock:
+            # spawn rock
+            self._spawn_rock()
+            # create unique key of the state for cycle detection
+            key = (*self._get_surface_profile(), wind_idx, self.falling_rock.type)
+            # append the key
+            self.state_cache[key].append((self.rock_counter, self.highetst))
+
+
+        # apply some wind
+        self.falling_rock.move(*self._get_direction_from_string(wind))
+
+        # has the rock collided?
+        if self._check_rock_collision(self.falling_rock):
+            self.falling_rock.move(*self._get_direction_from_string(wind, inverse = True))
+
+        # apply rock fall motion
+        self.falling_rock.move(*self._get_direction_from_string('v'))
+
+        # check whether the rock collided in this move
+        if self._check_rock_collision(self.falling_rock):
+            self.falling_rock.move(*self._get_direction_from_string('v', inverse = True))
+            self._place_rock(self.falling_rock)
+            placed = True
+
+        return placed
+
+def part1(rocks = 2022):
+    pattern = read_input()
+    cave = Cave(pattern)
+    while cave.rock_counter < rocks:
+        cave.step()
+    print(f'Part 1: the height after 2022 rocks is: ' + str(cave.highetst + 1))
+
+def part2(target = 1000000000000):
+    pattern = read_input()
+    cave = Cave(pattern)
+
+    # make these rocks fall to detect cycles
+    rocks = 3000
+    heights = []
+    while cave.rock_counter < rocks:
+        if cave.step():
+            heights.append(cave.highetst)
+
+    # detect cycles
+    (cycle_start, highest_start), cycle_size, height_per_cycle = cave.detect_cycles()
+    assert cycle_size != -1, 'No cycle...'
+    
+    # height before the first cycle
+    result = heights[cycle_start] 
+
+    # how many times do cycles get repeated
+    cycle_number, rest = divmod(target - cycle_start, cycle_size)
+
+    # compute the final result
+    result += cycle_number * height_per_cycle + (heights[cycle_start + rest] - heights[cycle_start])
+    print(f'Part 2: the height after 1000000000000 rocks is: ' + str(result))
+
+if __name__ == '__main__':
+    part1()
+    part2()
+
+
+
+
+
+
+    
 
 
 
--- main.py
+++ main.py
@@ -1,74 +1,228 @@
-import re
+import collections
 
-y_spot = 2000000 
-ys = set()
-beacons_y = set()
+def read_input(path: str = 'input.txt'):
+    with open(path) as file:
+        pattern = file.read().rstrip()
+    return pattern
 
-with open("input.txt", 'r') as file:
-    for ll in file.readlines():
-        # get sensor and corresponding closest beacon coordinates
-        sx, sy, bx, by = map(int, re.findall(r'(?<=\=)(.*?)(?=,|\:|\n)', ll))
+# pattern generator fun
+def pattern_generator(pattern: str):
+    idx = 0
+    n = len(pattern)
+    while True:
+        yield pattern[idx], idx
+        idx = (idx + 1) % n
 
-        if by == y_spot: # if the beacon is in the specified row
-            beacons_y.add(bx)
+# Rock class
+class Rock:
 
-        # compute the threshold distance
-        d = abs(bx - sx) + abs(by - sy)
-        d -= abs(y_spot - sy)
+    def __init__(self, rock_type: str, x_offset: int, y_offset: int = 2):
 
-        # add anything within the range
-        for x in range(sx - d, sx + d + 1):
-            ys.add(x)
+        # rock tye init ("-", "+", "L", "I", "o")
+        if rock_type == '-':
+            self.positions = [[x, y_offset] for x in range(x_offset, x_offset + 4)]
+        elif rock_type == '+':
+            self.positions = [[x_offset + 1, y_offset + 2], [x_offset, y_offset + 1], [x_offset + 1, y_offset + 1],
+            [x_offset + 2, y_offset + 1], [x_offset + 1, y_offset]]
+        elif rock_type == 'L':
+            self.positions = [[x_offset + 2, y_offset + 2], [x_offset + 2, y_offset + 1]] \
+                + [[x, y_offset] for x in range(x_offset, x_offset + 3)]
+        elif rock_type == 'I':
+            self.positions = [[x_offset, y] for y in range(y_offset + 3, y_offset - 1, -1)]
+        elif rock_type == 'o':
+            self.positions = [[x_offset, y_offset + 1], [x_offset + 1, y_offset + 1], [x_offset, y_offset],
+            [x_offset + 1, y_offset]]
+        else: 
+            raise NotImplementedError(f'{rock_type} is low key not recognisable.')
 
-print('Part 1: the solution is: ' + str(len(ys - beacons_y)))
+        # rock type
+        self.type = rock_type
 
-mx = 4000000
-y_ranges = [[] for _ in range(mx + 1)]
-with open("input.txt", 'r') as file:
-    for ll in file.readlines():
-        # get sensor and corresponding closest beacon coordinates
-        sx, sy, bx, by = map(int, re.findall(r'(?<=\=)(.*?)(?=,|\:|\n)', ll))
+    # fun to return the position in tuple
+    def get_positions(self) -> list[tuple[int]]:
+        return [tuple(position) for position in self.positions]
 
-        # get potential y ranges and corresponding x ranges
-        d = abs(bx - sx) + abs(by - sy)
-        dy = 0
-        while d > 0: # will countdown the distance
-            xl = max(0, sx - d)
-            xr = min(mx, sx + d)
-            if (sy - dy >= 0):
-                y_ranges[sy - dy].append([xl, xr])
-            if (sy + dy <= mx and dy):
-                y_ranges[sy + dy].append([xl, xr])
-            dy += 1
-            d -= 1
+    # fun to move the rock
+    def move(self, dx, dy):
+        for coords in self.positions: 
+            coords[0] += dx
+            coords[1] += dy
 
-    # the answer for y is in there somewehre
-    for ans_y in range(mx + 1):
-        xs = y_ranges[ans_y]
-        if not xs:
-            continue
-        xs.sort()
+# Cave class
+class Cave:
+    
+    def __init__(self, wind_pattern: str, rock_pattern: str = '-+LIo', width = 7):
+        self.width = width
+        self.occupied = set()
+        self.highetst = -1
+        self.falling_rock = None
+        self.wind_generator = pattern_generator(wind_pattern)
+        self.rock_type_generator = pattern_generator(rock_pattern)
+        self.rock_counter = 0
+        self.state_cache = collections.defaultdict(list) # store the state here for cycle detection
 
-        if xs[0][0] != 0:
-            ans_x = 0
-            break
+    # spawn rocks down the cave
+    def _spawn_rock(self):
+        self.falling_rock = Rock(next(self.rock_type_generator)[0], 2, self.highetst + 4)
 
-        # sorry guys -- bit tipsy right now :D 
-        last_e = xs[0][1]
-        for ii in range(1, len(xs)):
-            if last_e >= xs[ii][0] - 1:
-                last_e = max(last_e, xs[ii][1])
-            else:
+    # place rocks
+    def _place_rock(self, rock: Rock):
+        # rock position
+        positions = rock.get_positions()
+
+        # latest cave height
+        self.highetst = max(self.highetst, *[position[1] for position in positions])
+
+        # latest occupied rocks
+        self.occupied.update(positions)
+
+        # rock counter up 
+        self.rock_counter += 1
+
+        # no more falling rock
+        self.falling_rock = None
+
+    # has the rock colllided in the cave
+    def _check_rock_collision(self, rock: Rock):
+        return any(self._check_collision(*position) for position in rock.get_positions())
+
+    def _check_collision(self, x: int, y:int):
+        return x < 0 or x > self.width - 1 or y < 0 or (x, y) in self.occupied
+
+    # check if a row is completely blocked in the tetris style
+    def highest_row_blocked(self):
+        return all((x, self.highest) in self.occupied for x in range(0, self.width))
+
+    # fun to detect cycles
+    def detect_cycles(self) -> tuple[tuple[int, int], int, int]:
+        cycles = []
+
+        for value in self.state_cache.values():
+            if len(value) > 1:
+                cycles = value
                 break
 
-        if last_e != mx:
-            ans_x = last_e + 1
-            break
+        if not cycles:
+            return (-1, -1), -1, -1
 
-# can you hear my computer? running like crazy right now....
-print('Part 2: the solution is: ' + str(mx * ans_x + ans_y))   
+        return cycles[0], cycles[1][0] - cycles[0][0], cycles[1][1] - cycles[0][1]
 
-        
+    # get wind direction from strings
+    @staticmethod
+    def _get_direction_from_string(direction: str, inverse: bool = False):
+
+        # translate the direction if inverse
+        if inverse: 
+            if direction == '>':
+                direction = '<'
+            elif direction == '<':
+                direction = '>'
+            elif direction == 'v':
+                direction = '^'
+            elif direction == '^':
+                direction = 'v'
+            else:
+                raise NotImplementedError
+
+        # go through the cases
+        if direction == '>':
+            dx = 1
+            dy = 0
+        elif direction == '<':
+            dx = -1
+            dy = 0
+        elif direction == 'v':
+            dx = 0
+            dy = -1
+        elif direction == '^':
+            dx = 0
+            dy = 1
+        else: 
+            raise NotImplementedError
+
+        return dx, dy
+
+    def _get_surface_profile(self):
+        # the first element blocking each column
+        profile = []
+        for x in range(0, self.width):
+            y = self.highetst
+            while not self._check_collision(x, y):
+                y -= 1
+            profile.append(self.highetst - y)
+        return tuple(profile)
+
+    # implement the steps
+    def step(self):
+        # get the current wind
+        wind, wind_idx = next(self.wind_generator)
+
+        # init placed
+        placed = False
+
+        # do we have a falling rock or should one start fallling
+        if not self.falling_rock:
+            # spawn rock
+            self._spawn_rock()
+            # create unique key of the state for cycle detection
+            key = (*self._get_surface_profile(), wind_idx, self.falling_rock.type)
+            # append the key
+            self.state_cache[key].append((self.rock_counter, self.highetst))
+
+
+        # apply some wind
+        self.falling_rock.move(*self._get_direction_from_string(wind))
+
+        # has the rock collided?
+        if self._check_rock_collision(self.falling_rock):
+            self.falling_rock.move(*self._get_direction_from_string(wind, inverse = True))
+
+        # apply rock fall motion
+        self.falling_rock.move(*self._get_direction_from_string('v'))
+
+        # check whether the rock collided in this move
+        if self._check_rock_collision(self.falling_rock):
+            self.falling_rock.move(*self._get_direction_from_string('v', inverse = True))
+            self._place_rock(self.falling_rock)
+            placed = True
+
+        return placed
+
+def part1(rocks = 2022):
+    pattern = read_input()
+    cave = Cave(pattern)
+    while cave.rock_counter < rocks:
+        cave.step()
+    print(f'Part 1: the height after 2022 rocks is: ' + str(cave.highetst + 1))
+
+def part2(target = 1000000000000):
+    pattern = read_input()
+    cave = Cave(pattern)
+
+    # make these rocks fall to detect cycles
+    rocks = 3000
+    heights = []
+    while cave.rock_counter < rocks:
+        if cave.step():
+            heights.append(cave.highetst)
+
+    # detect cycles
+    (cycle_start, highest_start), cycle_size, height_per_cycle = cave.detect_cycles()
+    assert cycle_size != -1, 'No cycle...'
+    
+    # height before the first cycle
+    result = heights[cycle_start] 
+
+    # how many times do cycles get repeated
+    cycle_number, rest = divmod(target - cycle_start, cycle_size)
+
+    # compute the final result
+    result += cycle_number * height_per_cycle + (heights[cycle_start + rest] - heights[cycle_start])
+    print(f'Part 2: the height after 1000000000000 rocks is: ' + str(result))
+
+if __name__ == '__main__':
+    part1()
+    part2()
 
 
 
